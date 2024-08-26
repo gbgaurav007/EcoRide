c3b09612be0d0aa00c1ce78220ca979a932732ab
Gaurav Bansal <gaurav2014.be21@chitkara.edu.in>
2024-08-27 00:57:18 +0530

    updated env

--- frontend/src/ApiBaseURL.js
+++ frontend/src/ApiBaseURL.js
@@ -1,5 +1,6 @@
 // This is backend URL
 
-const API_BASE_URL = `${process.env.BACKEND_URL}/api/v1/`;
+
+const API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api/v1/` ;
 
 export default API_BASE_URL;
