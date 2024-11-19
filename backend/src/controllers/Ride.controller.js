4b3fa0d4dd928293cb3db1a9503b06deb498271b
Gaurav Bansal <gbgauravbansal007@gmail.com>
2024-11-19 18:49:31 +0530

    Updater Rides

--- backend/src/controllers/Ride.controller.js
+++ backend/src/controllers/Ride.controller.js
@@ -79,10 +79,6 @@ const searchRides = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, rides, "Ride searched successfully"));
 });
 
-
-
-
-
 const bookRide = asyncHandler(async (req, res) => {
   const { rideId } = req.body;
 
@@ -123,4 +119,16 @@ const bookRide = asyncHandler(async (req, res) => {
 });
 
 
-export { publishRide, searchRides, bookRide };
+const getSavedRides = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id).populate('savedRides');
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user.savedRides, "Saved rides fetched successfully"));
+});
+
+export { publishRide, searchRides, bookRide, getSavedRides};
